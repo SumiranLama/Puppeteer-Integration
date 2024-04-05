58d309479a8a30bd70b0b46b6fb77f4032ce7200
Sumiran Lama <sumiran.lama@yahoo.com>
2024-04-05 15:43:54 +0530

    Collecting data like title,image url,price and saving in a array

--- index.js
+++ index.js
@@ -2,12 +2,51 @@ import puppeteer from 'puppeteer';
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false
+    headless: false,
+    defaultViewport: false,
+    userDataDir: "./tmp"
   });
   const page = await browser.newPage();
-  await page.goto('https://www.google.com');
-  await page.screenshot({path: 'example.png'});
+  await page.goto('https://www.amazon.in/gp/bestsellers/boost/10894241031/ref=zg_bs_nav_boost_2_10894234031');
+  
+  const productsHandles = await page.$$('.p13n-sc-uncoverable-faceout');
+  //p13n-sc-uncoverable-faceout
+  //.p13n-gridRow ._cDEzb_grid-row_3Cywl
+//   console.log("Producthandles:",productsHandles);
+//.a-link-normal
+//_cDEzb_p13n-sc-css
+let items = [];
+
+
+  for(const productsHandle of productsHandles){
+    let title = "Null";
+    let price = "Null";
+    let image = "Null";
+        try {
+            title = await productsHandle.evaluate(el => el.querySelector("span > div").textContent, productsHandle)
+        } catch (error) {
+        }
+
+
+        try {
+            price = await productsHandle.evaluate(el => el.querySelector(".a-row > span").textContent, productsHandle)            
+        } catch (error) {
+        }
+
+        try {
+            image = await productsHandle.evaluate(el => el.querySelector(".p13n-product-image").getAttribute("src"), productsHandle)
+        } catch (error) {
+        }
+
+        if(title !=="Null" || price !=="Null" || image !=="Null" ){
+            items.push({title, price, image});
+        }
+  }
+
+  console.log(items);
+
+  
   
 
-  await browser.close();
+//   await browser.close();
 })();
